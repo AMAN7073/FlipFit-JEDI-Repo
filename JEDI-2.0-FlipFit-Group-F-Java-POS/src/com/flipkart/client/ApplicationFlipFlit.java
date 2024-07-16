dbd8a402a8aebaf331cdda542d20da42437eed41
Aman7073 <amanmittaljaipur@gmail.com>
2024-07-16 20:54:07 +0530

    First Commit

---
+++ JEDI-2.0-FlipFit-Group-F-Java-POS/src/com/flipkart/client/ApplicationFlipFlit.java
@@ -0,0 +1,7 @@
+package com.flipkart.client;
+
+public class ApplicationFlipFlit {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Flip-Fit Application");
+    }
+}
